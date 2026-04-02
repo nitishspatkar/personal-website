37b551c6f31ff051c68000e734899bdef30a3f22
nitishspatkar <nitishspatkar@gmail.com>
2026-04-02 08:21:25 +0200

    cvs updated

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ const defaultMetaDescription =
   site.params?.description || profile.subtitle || undefined;
 const socialIcons = site.params?.socialIcons || [];
 const gaId = site.params?.GoogleAnalyticsID;
-const cvPdfUrl = site.params?.cvPdfUrl || "/pdfs/cv/Patkar_cv_En.pdf";
+const cvPdfUrl = site.params?.cvPdfUrl || "/pdfs/cv/Patkar_CV.pdf";
 
 function loadCv() {
   try {
